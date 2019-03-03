9070881868893e306a415e7a0ab18007955f4b7c
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-03-03 15:17:18 +0100

    create basic language for BC

--- bc-truffle/src/main/java/BCLanguage.java
+++ bc-truffle/src/main/java/BCLanguage.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import com.oracle.truffle.api.TruffleLanguage;
+import runtime.BCContext;
 
-public class BCLanguage {
+public class BCLanguage extends TruffleLanguage<BCContext> {
+
+    public static final String ID = "bc";
+    public static final String MIME_TYPE = "application/x-bc";
+
+    protected BCContext createContext(Env env) {
+        // TODO
+        return null;
+    }
+
+    protected boolean isObjectOfLanguage(Object object) {
+        // TODO
+        return false;
+    }
 }
