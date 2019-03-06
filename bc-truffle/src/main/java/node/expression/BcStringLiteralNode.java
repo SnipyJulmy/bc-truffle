41c159ad3e20934e93a60e233678783cb9f3f6dd
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-03-06 11:33:38 +0100

    Add node for literal String

---
+++ bc-truffle/src/main/java/node/expression/BcStringLiteralNode.java
@@ -0,0 +1,25 @@
+package node.expression;
+
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.NodeInfo;
+import com.oracle.truffle.api.nodes.UnexpectedResultException;
+import node.BcExpressionNode;
+
+@NodeInfo(shortName = "const")
+public class BcStringLiteralNode extends BcExpressionNode {
+    private final String value;
+
+    public BcStringLiteralNode(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String executeString(VirtualFrame frame) throws UnexpectedResultException {
+        return value;
+    }
+
+    @Override
+    public String executeGeneric(VirtualFrame frame) {
+        return value;
+    }
+}
