0b72032e40e3189bfd31033a375bb90546df21eb
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-03-06 11:18:51 +0100

    complete the type system and add a TODO

--- bc-truffle/src/main/java/node/BcTypes.java
+++ bc-truffle/src/main/java/node/BcTypes.java
@@ -3,8 +3,10 @@ package node;
 import com.oracle.truffle.api.dsl.TypeSystem;
 
 @TypeSystem({
-        double.class,
-        String.class
+        double.class,  // bc only work with multi-precision value
+        String.class,  // support for string
+        Object[].class // only used to pass arguments for function call
+        // TODO : consider BigDouble, BigInteger, Long (when scale is 0 !)
 })
 public abstract class BcTypes {
 
