eea5fb0b75c6925fda0f8114a3f337abc65e3e2d
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-03-06 11:11:50 +0100

    add the executeVoid abstract method (for statement's node)

--- bc-truffle/src/main/java/node/BcStatementNode.java
+++ bc-truffle/src/main/java/node/BcStatementNode.java
@@ -1,11 +1,13 @@
 package node;
 
 import com.oracle.truffle.api.dsl.ReportPolymorphism;
+import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.nodes.NodeInfo;
 
 @NodeInfo(language = "BC", description = "abstract base node for any bc statement")
 @ReportPolymorphism
 public abstract class BcStatementNode extends Node {
-
+    // execute the node as a statement (no return value)
+    public abstract void executeVoid(VirtualFrame frame);
 }
