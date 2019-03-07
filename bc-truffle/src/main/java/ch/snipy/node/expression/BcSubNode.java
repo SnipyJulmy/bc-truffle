48fb8dc0e7a06f01fa33409a2a9d7a70b61fcf82
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-03-07 16:03:41 +0100

    Add node for Add,Sub,Mul and Div

---
+++ bc-truffle/src/main/java/ch/snipy/node/expression/BcSubNode.java
@@ -0,0 +1,20 @@
+package ch.snipy.node.expression;
+
+import ch.snipy.BcException;
+import ch.snipy.node.BcBinaryNode;
+import com.oracle.truffle.api.dsl.Fallback;
+import com.oracle.truffle.api.dsl.Specialization;
+
+public abstract class BcSubNode extends BcBinaryNode {
+
+    @Specialization
+    protected double doDouble(double left, double right) {
+        return left - right;
+    }
+
+
+    @Fallback
+    protected Object typeError(Object left, Object right) {
+        throw BcException.typeError(this, left, right);
+    }
+}
