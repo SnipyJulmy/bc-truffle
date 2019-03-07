18fa5653791e662ab9f8e85d9abd9a0030b8cd36
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-03-07 16:11:06 +0100

    Add skeleton for the statement's nodes

---
+++ bc-truffle/src/main/java/ch/snipy/node/statement/BcHaltNode.java
@@ -0,0 +1,6 @@
+package ch.snipy.node.statement;
+
+import ch.snipy.node.BcStatementNode;
+
+public abstract class BcHaltNode extends BcStatementNode {
+}
---
+++ bc-truffle/src/main/java/ch/snipy/node/statement/BcReturnNode.java
@@ -0,0 +1,6 @@
+package ch.snipy.node.statement;
+
+import ch.snipy.node.BcStatementNode;
+
+public abstract class BcReturnNode extends BcStatementNode {
+}
