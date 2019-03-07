5cb20197a9413e5604f472438ae68b3a8d1c012c
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-03-07 15:43:45 +0100

    add implementation for OR,AND,NOT

---
+++ bc-truffle/src/main/java/ch/snipy/node/expression/BcAndNode.java
@@ -0,0 +1,22 @@
+package ch.snipy.node.expression;
+
+import ch.snipy.BcException;
+import ch.snipy.node.BcBinaryNode;
+import com.oracle.truffle.api.dsl.Fallback;
+import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.nodes.NodeInfo;
+
+@NodeInfo(shortName = "&&")
+public abstract class BcAndNode extends BcBinaryNode {
+
+    @Specialization
+    protected boolean and(boolean left, boolean right) {
+        return left && right;
+    }
+
+    @Fallback
+    protected Object typeError(Object left, Object right) {
+        throw BcException.typeError(this, left, right);
+    }
+
+}
--- bc-truffle/src/main/java/node/expression/BcNotNode.java
+++
@@ -1,8 +0,0 @@
-package node.expression;
-
-import node.BcUnaryNode;
-import org.graalvm.compiler.nodeinfo.NodeInfo;
-
-@NodeInfo(shortName = "!")
-public abstract class BcNotNode extends BcUnaryNode {
-}
