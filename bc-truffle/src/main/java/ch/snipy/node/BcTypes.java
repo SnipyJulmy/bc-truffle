6d1190f1971ba2f0d80cb22518e300013634bcd7
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-03-07 16:05:20 +0100

    update the type system

--- bc-truffle/src/main/java/ch/snipy/node/BcTypes.java
+++ bc-truffle/src/main/java/ch/snipy/node/BcTypes.java
@@ -1,8 +1,9 @@
-package node;
+package ch.snipy.node;
 
 import com.oracle.truffle.api.dsl.TypeSystem;
 
 @TypeSystem({
+        boolean.class, // for boolean expression, in bc a true == 1 and false == 0
         double.class,  // bc only work with multi-precision value
         String.class,  // support for string
         Object[].class // only used to pass arguments for function call
