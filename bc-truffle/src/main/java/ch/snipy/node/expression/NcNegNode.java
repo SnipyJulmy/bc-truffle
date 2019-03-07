4889b23863e07b2753bbcc19bfdbce8a397c0453
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-03-07 16:13:12 +0100

    Add a NegNode for unary negation

---
+++ bc-truffle/src/main/java/ch/snipy/node/expression/NcNegNode.java
@@ -0,0 +1,20 @@
+package ch.snipy.node.expression;
+
+import ch.snipy.BcException;
+import ch.snipy.node.BcUnaryNode;
+import com.oracle.truffle.api.dsl.Fallback;
+import com.oracle.truffle.api.dsl.Specialization;
+
+public abstract class NcNegNode extends BcUnaryNode {
+
+    @Specialization
+    protected double doDouble(double value) {
+        return -value;
+    }
+
+
+    @Fallback
+    protected Object typeError(Object value) {
+        throw BcException.typeError(this, value);
+    }
+}
