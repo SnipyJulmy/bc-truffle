dea154ba220f1f1cbeb5bc7ebe398e1c36d4e9be
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-03-13 14:07:30 +0100

    implement the for node and the for repeating node

--- bc-truffle/src/main/java/ch/snipy/node/statement/BcForNode.java
+++ bc-truffle/src/main/java/ch/snipy/node/statement/BcForNode.java
@@ -1,6 +1,22 @@
 package ch.snipy.node.statement;
 
+import ch.snipy.node.BcExpressionNode;
 import ch.snipy.node.BcStatementNode;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.LoopNode;
 
-public abstract class BcForNode extends BcStatementNode {
+public final class BcForNode extends BcStatementNode {
+    @Child private LoopNode loopNode;
+    @Child private BcExpressionNode initNode;
+
+    public BcForNode(LoopNode loopNode, BcExpressionNode initNode) {
+        this.loopNode = loopNode;
+        this.initNode = initNode;
+    }
+
+    @Override
+    public void executeVoid(VirtualFrame frame) {
+        initNode.executeVoid(frame);
+        loopNode.executeLoop(frame);
+    }
 }
