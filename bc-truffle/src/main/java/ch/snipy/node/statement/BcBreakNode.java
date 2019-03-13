e81210452b4a42dbe5ae8313f505337bb2a1ac26
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-03-13 14:47:29 +0100

    implement the break and continue statement

--- bc-truffle/src/main/java/ch/snipy/node/statement/BcBreakNode.java
+++ bc-truffle/src/main/java/ch/snipy/node/statement/BcBreakNode.java
@@ -1,6 +1,15 @@
 package ch.snipy.node.statement;
 
 import ch.snipy.node.BcStatementNode;
+import ch.snipy.node.controlflow.BcBreakException;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.NodeInfo;
 
-public abstract class BcBreakNode extends BcStatementNode {
+@NodeInfo(shortName = "break", description = "node implementing a break statement")
+public final class BcBreakNode extends BcStatementNode {
+
+    @Override
+    public void executeVoid(VirtualFrame frame) {
+        throw BcBreakException.SINGLETON;
+    }
 }
--- bc-truffle/src/main/java/ch/snipy/node/statement/BcContinueNode.java
+++ bc-truffle/src/main/java/ch/snipy/node/statement/BcContinueNode.java
@@ -1,6 +1,15 @@
 package ch.snipy.node.statement;
 
 import ch.snipy.node.BcStatementNode;
+import ch.snipy.node.controlflow.BcContinueException;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.NodeInfo;
 
-public abstract class BcContinueNode extends BcStatementNode {
+@NodeInfo(shortName = "continue", description = "node implementing a continue statement")
+public final class BcContinueNode extends BcStatementNode {
+
+    @Override
+    public void executeVoid(VirtualFrame frame) {
+        throw BcContinueException.SINGLETON;
+    }
 }
