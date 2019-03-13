792ab0814f63fb393f35b4e21f2558b85f202f35
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-03-13 13:08:19 +0100

    add some comments

--- bc-truffle/src/main/java/ch/snipy/node/BcRootNode.java
+++ bc-truffle/src/main/java/ch/snipy/node/BcRootNode.java
@@ -4,9 +4,16 @@ package ch.snipy.node;
 import com.oracle.truffle.api.TruffleLanguage;
 import com.oracle.truffle.api.frame.FrameDescriptor;
 import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.NodeInfo;
 import com.oracle.truffle.api.nodes.RootNode;
 
+@NodeInfo(language = "bc", description = "root node for all BC execution tree")
 public class BcRootNode extends RootNode {
+
+    // the statement to execute, body is either :
+    // - a list block statement, which is a list of statement
+    // - a function body which is also a list of statement
+    @Child private BcStatementNode body;
 
     protected BcRootNode(TruffleLanguage<?> language) {
         super(language);
