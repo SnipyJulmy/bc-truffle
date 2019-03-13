18e1fe68761fb66d409c925701749c735ca880be
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-03-13 15:00:07 +0100

    fix format

--- bc-truffle/src/main/java/ch/snipy/node/controlflow/BcBreakException.java
+++ bc-truffle/src/main/java/ch/snipy/node/controlflow/BcBreakException.java
@@ -4,6 +4,7 @@ import com.oracle.truffle.api.nodes.ControlFlowException;
 
 public final class BcBreakException extends ControlFlowException {
     public static final BcBreakException SINGLETON = new BcBreakException();
+
     private BcBreakException() {
     }
 }
--- bc-truffle/src/main/java/ch/snipy/node/controlflow/BcContinueException.java
+++ bc-truffle/src/main/java/ch/snipy/node/controlflow/BcContinueException.java
@@ -4,6 +4,7 @@ import com.oracle.truffle.api.nodes.ControlFlowException;
 
 public final class BcContinueException extends ControlFlowException {
     public static final BcContinueException SINGLETON = new BcContinueException();
+
     private BcContinueException() {
     }
 }
