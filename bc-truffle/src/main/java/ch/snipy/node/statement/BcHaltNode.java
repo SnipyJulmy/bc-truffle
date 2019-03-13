3877eeed4749ebb2214cc9fd9c7f62ebe4d5ac25
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-03-13 15:00:17 +0100

    implement the halt node

--- bc-truffle/src/main/java/ch/snipy/node/statement/BcHaltNode.java
+++ bc-truffle/src/main/java/ch/snipy/node/statement/BcHaltNode.java
@@ -1,6 +1,14 @@
 package ch.snipy.node.statement;
 
 import ch.snipy.node.BcStatementNode;
+import ch.snipy.node.controlflow.BcHaltException;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.NodeInfo;
 
-public abstract class BcHaltNode extends BcStatementNode {
+@NodeInfo(shortName = "halt", description = "node representing the halt of the program")
+public final class BcHaltNode extends BcStatementNode {
+    @Override
+    public void executeVoid(VirtualFrame frame) {
+        throw BcHaltException.SINGLETON;
+    }
 }
