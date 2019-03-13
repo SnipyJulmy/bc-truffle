e031bdf23b1121a406f2d98bd29cf97e3519e7ee
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-03-13 13:36:20 +0100

    add exception for break and continue statements

---
+++ bc-truffle/src/main/java/ch/snipy/node/controlflow/BcBreakException.java
@@ -0,0 +1,9 @@
+package ch.snipy.node.controlflow;
+
+import com.oracle.truffle.api.nodes.ControlFlowException;
+
+public final class BcBreakException extends ControlFlowException {
+    public static final BcBreakException SINGLETON = new BcBreakException();
+    private BcBreakException() {
+    }
+}
