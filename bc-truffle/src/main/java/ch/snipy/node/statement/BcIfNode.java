0c4cd740fac72f02d4d3c724da66ee2ea0c9a781
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-03-13 13:07:52 +0100

    implement the IfNode with condition profile

--- bc-truffle/src/main/java/ch/snipy/node/statement/BcIfNode.java
+++ bc-truffle/src/main/java/ch/snipy/node/statement/BcIfNode.java
@@ -1,6 +1,33 @@
 package ch.snipy.node.statement;
 
+import ch.snipy.BcException;
+import ch.snipy.node.BcExpressionNode;
 import ch.snipy.node.BcStatementNode;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.UnexpectedResultException;
+import com.oracle.truffle.api.profiles.ConditionProfile;
 
-public abstract class BcIfNode extends BcStatementNode {
+public final class BcIfNode extends BcStatementNode {
+    final ConditionProfile conditionProfile = ConditionProfile.createCountingProfile();
+    @Child private BcExpressionNode conditionNode;
+    @Child private BcStatementNode trueNode;
+    @Child private BcStatementNode falseNode;
+
+    public BcIfNode(BcExpressionNode conditionNode, BcStatementNode trueNode, BcStatementNode falseNode) {
+        this.conditionNode = conditionNode;
+        this.trueNode = trueNode;
+        this.falseNode = falseNode;
+    }
+
+    @Override
+    public void executeVoid(VirtualFrame frame) {
+        try {
+            if (conditionProfile.profile(conditionNode.executeBoolean(frame)))
+                trueNode.executeVoid(frame);
+            else
+                falseNode.executeVoid(frame);
+        } catch (UnexpectedResultException e) {
+            throw BcException.typeError(this, e.getResult());
+        }
+    }
 }
