e3e00cf9552b62cc06d2519e1b37116ed686a3cd
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-03-13 13:38:41 +0100

    implements the while node

--- bc-truffle/src/main/java/ch/snipy/node/statement/BcWhileNode.java
+++ bc-truffle/src/main/java/ch/snipy/node/statement/BcWhileNode.java
@@ -1,6 +1,22 @@
 package ch.snipy.node.statement;
 
+import ch.snipy.node.BcExpressionNode;
 import ch.snipy.node.BcStatementNode;
+import com.oracle.truffle.api.Truffle;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.LoopNode;
 
-public abstract class BcWhileNode extends BcStatementNode {
+public final class BcWhileNode extends BcStatementNode {
+    @Child private LoopNode loopNode;
+
+    public BcWhileNode(BcExpressionNode conditionNode, BcStatementNode bodyNode) {
+        this.loopNode = Truffle.getRuntime().createLoopNode(
+                new BcWhileRepeatingNode(conditionNode, bodyNode)
+        );
+    }
+
+    @Override
+    public void executeVoid(VirtualFrame frame) {
+        loopNode.executeLoop(frame);
+    }
 }
