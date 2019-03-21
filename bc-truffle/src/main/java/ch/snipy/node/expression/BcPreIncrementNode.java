cf0194e40e1eae3b39dbe4e4bf8e0deab533fd73
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-03-21 15:31:47 +0100

    add node for pre increment operation

---
+++ bc-truffle/src/main/java/ch/snipy/node/expression/BcPreIncrementNode.java
@@ -0,0 +1,27 @@
+package ch.snipy.node.expression;
+
+import ch.snipy.node.BcExpressionNode;
+import com.oracle.truffle.api.dsl.NodeField;
+import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.frame.FrameSlot;
+import com.oracle.truffle.api.frame.FrameSlotKind;
+import com.oracle.truffle.api.frame.FrameUtil;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.UnexpectedResultException;
+
+@NodeField(name = "slot", type = FrameSlot.class)
+@NodeField(name = "modifier", type = double.class)
+public abstract class BcPreIncrementNode extends BcExpressionNode {
+
+    protected abstract FrameSlot getSlot();
+
+    protected abstract double getModifier();
+
+    @Specialization
+    public double doDouble(VirtualFrame frame) {
+        double value = FrameUtil.getDoubleSafe(frame, getSlot());
+        double newValue = value + getModifier();
+        frame.setDouble(getSlot(), newValue);
+        return newValue;
+    }
+}
