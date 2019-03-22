f58e82e258718af61a30dfd1b4f4be83deb7cb41
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-03-22 17:18:04 +0100

    add process to build the native image

---
+++ bc-native/clean_native.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+[ -f bcnative ] && rm bcnative || echo "bcnative not found."
---
+++ bc-native/make_native.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+if [[ $BC_BUILD_NATIVE == "false" ]]; then
+    echo "Skipping the native image build because BC_BUILD_NATIVE is set to false."
+    exit 0
+fi
+"$JAVA_HOME"/bin/native-image --tool:truffle -H:MaxRuntimeCompileMethods=1200 \
+    -cp ../language/target/bc-truffle-0.1.jar:../launcher/target/launcher-0.1.jar \
+    com.oracle.truffle.bc.launcher.BcMain \
+    bcnative
