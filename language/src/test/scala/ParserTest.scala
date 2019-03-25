143d5a1888c47003c277fbcdd27217dd8465cd4d
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-03-25 16:11:28 +0100

    skeleton for future parser unit test

--- language/src/test/scala/ParserTest.scala
+++ language/src/test/scala/ParserTest.scala
@@ -1,49 +1,12 @@
 class ParserTest extends BcTestSpec {
-
   behavior of "bc parser"
 
-  it should "correctly parse and evaluate double literals" in {
-    context.eval(mkSource("1.0")).asDouble() shouldBe 1.0
-    context.eval(mkSource("2.0")).asDouble() shouldBe 2.0
-    context.eval(mkSource("3.0")).asDouble() shouldBe 3.0
-    context.eval(mkSource("1.2")).asDouble() shouldBe 1.2
-    context.eval(mkSource("100.232")).asDouble() shouldBe 100.232
-    context.eval(mkSource("3.14")).asDouble() shouldBe 3.14
-    context.eval(mkSource("0")).asDouble() shouldBe 0.0
-    context.eval(mkSource("0.0")).asDouble() shouldBe 0.0
-  }
-
-  it should "correctly parse and evaluate term expression" in {
-    context.eval(mkSource("1 + 1")).asDouble() shouldBe 2.0
-    context.eval(mkSource("1 - 0")).asDouble() shouldBe 1.0
-    context.eval(mkSource("0 - 1")).asDouble() shouldBe -1.0
-    context.eval(mkSource("1 + 0")).asDouble() shouldBe 1.0
-    context.eval(mkSource("0 + 1")).asDouble() shouldBe 1.0
-    context.eval(mkSource("3 + 3 + 3 + 3")).asDouble() shouldBe 12.0
-  }
-
-  it should "correctly parse and evaluate factor expression" in {
-    context.eval(mkSource("1 * 2")).asDouble() shouldBe 2.0
-    context.eval(mkSource("3 * 2")).asDouble() shouldBe 6.0
-    context.eval(mkSource("1 * -2")).asDouble() shouldBe -2.0
-    context.eval(mkSource("56 * 72 + 1")).asDouble() shouldBe (56 * 72 + 1).toDouble
-    context.eval(mkSource("3 * 4 * 5 * 6 * 7")).asDouble() shouldBe (3 * 4 * 5 * 6 * 7).toDouble
-    context.eval(mkSource("12.3 * -56.7")).asDouble() shouldBe (12.3 * -56.7)
-    context.eval(mkSource("3 + 4 * 6 + 1 * 45 * 3 * -9 + 1 - 123 * 21")).asDouble() shouldBe 3 + 4 * 6 + 1 * 45 * 3 * -9 + 1 - 123 * 21
-  }
-
-  it should "correctly parse and evaluate unary operator expression" in {
-    context.eval(mkSource("-23")).asDouble() shouldBe -23.0
-    context.eval(mkSource("+23")).asDouble() shouldBe 23.0
-    context.eval(mkSource("+0")).asDouble() shouldBe 0.0
-    context.eval(mkSource("-0")).asDouble() shouldBe 0.0
-  }
-
-  it should "correctly parse and evaluate power expression " in {
-    context.eval(mkSource("2^4")).asDouble() shouldBe Math.pow(2.0, 4.0)
-    context.eval(mkSource("2^5")).asDouble() shouldBe Math.pow(2.0, 5.0)
-    context.eval(mkSource("2^6")).asDouble() shouldBe Math.pow(2.0, 6.0)
-    context.eval(mkSource("3^4")).asDouble() shouldBe Math.pow(3.0, 4.0)
-    context.eval(mkSource("2.2^4.9")).asDouble() shouldBe Math.pow(2.2, 4.9)
-  }
+  it should "correctly parse string literal" in {}
+  it should "correctly parse double literal" in {}
+  it should "correctly parse term expression" in {}
+  it should "correctly parse factor expression" in {}
+  it should "correctly parse power expression" in {}
+  it should "correctly parse neg (unary) expression" in {}
+  it should "correctly parse post/pre inc/dec expression" in {}
+  it should "correctly parse parenthesis expression" in {}
 }
