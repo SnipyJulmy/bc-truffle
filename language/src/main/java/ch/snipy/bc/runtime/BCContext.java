b0aba421bb18290732abce9f4bc7d7497c7a07b8
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-04-01 17:59:05 +0200

    add conversion from foreign value and number

--- language/src/main/java/ch/snipy/bc/runtime/BCContext.java
+++ language/src/main/java/ch/snipy/bc/runtime/BCContext.java
@@ -6,10 +6,14 @@ import ch.snipy.bc.builtins.BcPrintBuiltinFactory;
 import ch.snipy.bc.node.BcExpressionNode;
 import ch.snipy.bc.node.BcRootNode;
 import ch.snipy.bc.node.local.BcReadArgumentNode;
+import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.TruffleLanguage;
 import com.oracle.truffle.api.TruffleLanguage.Env;
 import com.oracle.truffle.api.dsl.NodeFactory;
 import com.oracle.truffle.api.frame.FrameDescriptor;
+import com.oracle.truffle.api.interop.TruffleObject;
+import com.oracle.truffle.api.nodes.NodeInfo;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -33,6 +37,35 @@ public final class BCContext {
         installBuiltins();
     }
 
+    public static Object fromForeignValue(Object o) {
+        if (o instanceof BcBigNumber || o instanceof String) return o;
+        else if (o instanceof Character) return String.valueOf(o);
+        else if (o instanceof Number) return fromForeignNumber(o);
+        else if (o instanceof TruffleObject) return o;
+        else if (o instanceof BCContext) return o;
+        CompilerDirectives.transferToInterpreter();
+        throw new IllegalStateException(o + " is not a Truffle value");
+    }
+
+    @TruffleBoundary
+    private static BcBigNumber fromForeignNumber(Object o) {
+        return new BcBigNumber(
+                ((Number) o).doubleValue()
+        );
+    }
+
+    public static NodeInfo lookupNodeInfo(Class<?> clazz) {
+        if (clazz == null) {
+            return null;
+        }
+        NodeInfo info = clazz.getAnnotation(NodeInfo.class);
+        if (info != null) {
+            return info;
+        } else {
+            return lookupNodeInfo(clazz.getSuperclass());
+        }
+    }
+
     private void installBuiltins() {
         installBuiltin(BcPrintBuiltinFactory.getInstance());
     }
@@ -44,8 +77,9 @@ public final class BCContext {
             args[i] = new BcReadArgumentNode(i);
         }
         BcBuiltinNode builtinNode = factory.createNode((Object) args);
-        BcRootNode rootNode = new BcRootNode(language, new FrameDescriptor(), builtinNode);
-        // todo register the builtin inside the registry
+        String name = lookupNodeInfo(builtinNode.getClass()).shortName();
+        BcRootNode rootNode = new BcRootNode(language, new FrameDescriptor(), builtinNode, name);
+        // TODO register the builtin inside the registry
     }
 
     public BufferedReader getInput() {
