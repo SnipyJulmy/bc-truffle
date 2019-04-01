8d7bb0249c060e64d50d7a94f54b04fc1c5cf1e1
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-04-01 14:30:02 +0200

    add BcBigNumber in the type system

--- language/src/main/java/ch/snipy/bc/node/BcTypes.java
+++ language/src/main/java/ch/snipy/bc/node/BcTypes.java
@@ -1,11 +1,12 @@
 package ch.snipy.bc.node;
 
+import ch.snipy.bc.runtime.BcBigNumber;
 import com.oracle.truffle.api.dsl.TypeSystem;
 
 import java.math.BigDecimal;
 
 @TypeSystem({
-        BigDecimal.class,   // standard posix bc number
+        BcBigNumber.class,   // standard posix bc number
         String.class,       // support for string
         Object[].class      // only used to pass arguments for function call
 })
