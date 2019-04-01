51b6c1ac8e118d23a7155bd11a94ad8227eb336c
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-04-01 16:08:13 +0200

    use nice expression

--- language/src/main/scala/ch/snipy/bc/Main.scala
+++ language/src/main/scala/ch/snipy/bc/Main.scala
@@ -3,8 +3,7 @@ package ch.snipy.bc
 import org.graalvm.polyglot.{Context, Source}
 
 object Main extends App {
-  val program =
-    """1 || 0""".stripMargin
+  val program = """(8866128975287528 ^ 3) + (-8778405442862239 ^ 3) + (-2736111468807040 ^ 3)""".stripMargin
 
   val source: Source = Source.newBuilder("bc", program, "program")
     .build()
