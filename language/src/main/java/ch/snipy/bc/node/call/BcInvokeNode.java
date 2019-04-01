7c6dd225235ef36aeae43f4dee3f823f1c49df8e
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-04-01 18:21:11 +0200

    add invoke node with dispatch node

--- language/src/main/java/ch/snipy/bc/node/call/BcInvokeNode.java
+++ language/src/main/java/ch/snipy/bc/node/call/BcInvokeNode.java
@@ -1,34 +1,32 @@
 package ch.snipy.bc.node.call;
 
 import ch.snipy.bc.node.BcExpressionNode;
+import com.oracle.truffle.api.CompilerAsserts;
 import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.ExplodeLoop;
+
+import java.util.Arrays;
 
 public final class BcInvokeNode extends BcExpressionNode {
 
     @Children private final BcExpressionNode[] argumentNodes;
     @Child private BcExpressionNode functionNode;
+    @Child private BcDispatchNode dispatchNode;
 
     public BcInvokeNode(BcExpressionNode functionNode, BcExpressionNode[] argumentsNode) {
         this.functionNode = functionNode;
         this.argumentNodes = argumentsNode;
+        this.dispatchNode = BcDispatchNodeGen.create();
     }
 
-    @Override
-    public Object executeGeneric(VirtualFrame frame) {
-        return null;
-    }
-
-    /* TODO
     @Override
     @ExplodeLoop
     public Object executeGeneric(VirtualFrame frame) {
         Object function = functionNode.executeGeneric(frame);
         CompilerAsserts.compilationConstant(argumentNodes.length);
-
-        Object[] argumentValues = new Object[argumentNodes.length];
-        for (int i = 0; i < argumentNodes.length; i++) {
-            argumentValues[i] = argumentNodes[i].executeGeneric(frame);
-        }
+        return dispatchNode.executeDispatch(
+                function,
+                Arrays.stream(argumentNodes).map(n -> n.executeGeneric(frame)).toArray()
+        );
     }
-    */
 }
