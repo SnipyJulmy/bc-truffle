74e94dc9be5dd469df1580df6c635b559e77d748
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-04-01 18:20:08 +0200

    add bcfunction type

---
+++ language/src/main/java/ch/snipy/bc/runtime/BcFunction.java
@@ -0,0 +1,51 @@
+package ch.snipy.bc.runtime;
+
+import ch.snipy.bc.BcLanguage;
+import ch.snipy.bc.node.BcUnderfinedFunctionRootNode;
+import com.oracle.truffle.api.Assumption;
+import com.oracle.truffle.api.RootCallTarget;
+import com.oracle.truffle.api.Truffle;
+import com.oracle.truffle.api.interop.ForeignAccess;
+import com.oracle.truffle.api.interop.TruffleObject;
+import com.oracle.truffle.api.utilities.CyclicAssumption;
+
+public final class BcFunction implements TruffleObject {
+
+    private final String name;
+    private final CyclicAssumption callTargetStable;
+
+    private RootCallTarget callTarget;
+
+    protected BcFunction(BcLanguage language, String name) {
+        this.name = name;
+        this.callTarget = Truffle.getRuntime().createCallTarget(new BcUnderfinedFunctionRootNode(language, name));
+        this.callTargetStable = new CyclicAssumption(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public RootCallTarget getCallTarget() {
+        return callTarget;
+    }
+
+    public Assumption getCallTargetStable() {
+        return callTargetStable.getAssumption();
+    }
+
+    protected void setCallTarget(RootCallTarget callTarget) {
+        this.callTarget = callTarget;
+        callTargetStable.invalidate();
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public ForeignAccess getForeignAccess() {
+        return BcFunctionMessageResolutionForeign.ACCESS;
+    }
+}
