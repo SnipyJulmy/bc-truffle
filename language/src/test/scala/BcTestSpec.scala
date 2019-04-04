acb3af1e769a59abd1c60a2a17f9d3ec9abb3402
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-04-04 13:55:54 +0200

    redirect the output of context to "test.out"

--- language/src/test/scala/BcTestSpec.scala
+++ language/src/test/scala/BcTestSpec.scala
@@ -1,13 +1,18 @@
+import java.io.FileOutputStream
+
 import org.graalvm.polyglot.{Context, Source}
 import org.scalatest.{FlatSpec, Matchers}
 import org.scalatestplus.scalacheck.Checkers
 
 trait BcTestSpec extends FlatSpec with Checkers with Matchers {
 
-  protected val context: Context = Context.newBuilder("bc")
-    .in(System.in)
-    .out(System.out)
-    .build()
+  protected val context: Context = {
+    val outputFile = "test.out"
+    Context.newBuilder("bc")
+      .in(System.in)
+      .out(new FileOutputStream(outputFile))
+      .build()
+  }
 
   protected def mkSource(source: String, name: String = "???"): Source =
     Source.newBuilder("bc", source, name).build()
