f96f92d338a4cb90d6fbea799ebaa21931175ec1
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-04-04 12:54:16 +0200

    Add a function literal node

---
+++ language/src/main/java/ch/snipy/bc/node/expression/BcFunctionLiteralNode.java
@@ -0,0 +1,35 @@
+package ch.snipy.bc.node.expression;
+
+import ch.snipy.bc.BcLanguage;
+import ch.snipy.bc.node.BcExpressionNode;
+import ch.snipy.bc.runtime.BCContext;
+import ch.snipy.bc.runtime.BcFunction;
+import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.TruffleLanguage.ContextReference;
+import com.oracle.truffle.api.frame.VirtualFrame;
+
+import static com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
+
+public final class BcFunctionLiteralNode extends BcExpressionNode {
+
+    private final String identifier;
+
+    @CompilationFinal
+    private BcFunction cachedFunction;
+
+    private final ContextReference<BCContext> reference;
+
+    public BcFunctionLiteralNode(BcLanguage language, String identifier) {
+        this.identifier = identifier;
+        this.reference = language.getContextReference();
+    }
+
+    @Override
+    public BcFunction executeGeneric(VirtualFrame frame) {
+        if (cachedFunction == null) {
+            CompilerDirectives.transferToInterpreterAndInvalidate();
+            cachedFunction = reference.get().getFunctionRegistry().lookup(identifier, true);
+        }
+        return cachedFunction;
+    }
+}
