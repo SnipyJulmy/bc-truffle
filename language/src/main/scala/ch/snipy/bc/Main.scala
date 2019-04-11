076fd5992eee909200dcfa97ec868439642fa738
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-04-11 12:51:29 +0200

    use a main which has to print "2"

--- language/src/main/scala/ch/snipy/bc/Main.scala
+++ language/src/main/scala/ch/snipy/bc/Main.scala
@@ -5,8 +5,10 @@ import org.graalvm.polyglot.{Context, Source}
 object Main extends App {
   val program =
     """
-       a = 20 ; while(--a > 0) print(a)
-       a = 20 ; while(a-- > 0) print(a)
+       define id(x) {
+         return x
+       }
+       print(id(2))
     """.stripMargin
 
   val source: Source = Source.newBuilder("bc", program, "program")
