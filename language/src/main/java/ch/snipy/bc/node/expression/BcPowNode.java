2efe82e3a8d1b2dd7f5c53d49a0eb0f654cd6f34
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-04-16 16:25:31 +0200

    add truffle boundary

--- language/src/main/java/ch/snipy/bc/node/expression/BcPowNode.java
+++ language/src/main/java/ch/snipy/bc/node/expression/BcPowNode.java
@@ -3,6 +3,8 @@ package ch.snipy.bc.node.expression;
 import ch.snipy.bc.BcException;
 import ch.snipy.bc.node.BcBinaryNode;
 import ch.snipy.bc.runtime.BcBigNumber;
+import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.dsl.Fallback;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.nodes.NodeInfo;
@@ -12,6 +14,11 @@ public abstract class BcPowNode extends BcBinaryNode {
 
     @Specialization
     protected BcBigNumber doDouble(BcBigNumber left, BcBigNumber right) {
+        return getBcBigNumber(left, right);
+    }
+
+    @TruffleBoundary
+    private BcBigNumber getBcBigNumber(BcBigNumber left, BcBigNumber right) {
         return left.pow(right);
     }
 
