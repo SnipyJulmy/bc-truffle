0dae54e93b591f43e62dfd48d086f6b0f5274da3
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-05-06 09:12:49 +0200

    add todo and use 0 by default

--- language/src/main/java/ch/snipy/bc/node/expression/BcPreIncrementNode.java
+++ language/src/main/java/ch/snipy/bc/node/expression/BcPreIncrementNode.java
@@ -9,6 +9,7 @@ import com.oracle.truffle.api.frame.FrameUtil;
 import com.oracle.truffle.api.frame.VirtualFrame;
 
 import static ch.snipy.bc.runtime.BcBigNumber.ONE;
+import static ch.snipy.bc.runtime.BcBigNumber.ZERO;
 
 @NodeField(name = "slot", type = FrameSlot.class)
 @NodeField(name = "modifier", type = double.class)
@@ -18,10 +19,14 @@ public abstract class BcPreIncrementNode extends BcExpressionNode {
 
     protected abstract double getModifier();
 
+    // TODO search in the upper context
     @SuppressWarnings("Duplicates")
     @Specialization
     public BcBigNumber doBcBigNumber(VirtualFrame frame) {
         BcBigNumber value = (BcBigNumber) FrameUtil.getObjectSafe(frame, getSlot());
+        if (value == null) {
+            value = ZERO;
+        }
         BcBigNumber newValue = value.add(getModifier() > 0.0 ? ONE : ONE.negate());
         frame.setObject(getSlot(), newValue);
         return newValue;
