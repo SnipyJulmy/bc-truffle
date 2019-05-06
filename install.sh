92622e5dd631a5b0964a88e75510f350d675433a
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-05-06 14:49:19 +0200

    separate package and install script

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+sudo gu uninstall ch.snipy.bc &&
+sudo gu -L install component/bc-component.jar
--- package.sh
+++ package.sh
@@ -4,6 +4,4 @@ mvn clean &&
 sed -i -e 's/RootNode root = BCParser$.MODULE$.parse(this, source);/RootNode root = null;/' $LANGFILE &&
 mvn compile &&
 sed -i -e 's/RootNode root = null;/RootNode root = BCParser$.MODULE$.parse(this, source);/' $LANGFILE &&
-mvn package &&
-sudo gu uninstall ch.snipy.bc &&
-sudo gu -L install component/bc-component.jar
+mvn package
