46f6e2bd2147b62263ffcb572cf3734b5f54b952
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-05-06 14:39:49 +0200

    update component creation

--- component/make_component.sh
+++ component/make_component.sh
@@ -2,44 +2,50 @@
 
 COMPONENT_DIR="component_temp_dir"
 LANGUAGE_PATH="$COMPONENT_DIR/jre/languages/bc"
-if [[ -f ../native/bcnative ]]; then
+INCLUDE_SLNATIVE="FALSE"
+
+if [[ -f ./native/bcnative ]]; then
     INCLUDE_SLNATIVE="TRUE"
 fi
 
 rm -rf COMPONENT_DIR
 
 mkdir -p "$LANGUAGE_PATH"
-sudo cp ../language/target/bc-truffle-0.1-jar-with-dependencies.jar "$LANGUAGE_PATH"
+cp ../language/target/bc-truffle-0.1-jar-with-dependencies.jar "$LANGUAGE_PATH"
 
 mkdir -p "$LANGUAGE_PATH/launcher"
-sudo cp ../launcher/target/bc-launcher.jar "$LANGUAGE_PATH/launcher/"
+cp ../launcher/target/bc-launcher.jar "$LANGUAGE_PATH/launcher/"
 
 mkdir -p "$LANGUAGE_PATH/bin"
-cp ../bc $LANGUAGE_PATH/bin/
-if [[ $INCLUDE_SLNATIVE = "TRUE" ]]; then
-    cp ../native/bcnative $LANGUAGE_PATH/bin/
+cp ../bc ${LANGUAGE_PATH}/bin/
+if [[ ${INCLUDE_SLNATIVE} = "TRUE" ]]; then
+    cp ../native/bcnative ${LANGUAGE_PATH}/bin/
 fi
 
 mkdir -p "$COMPONENT_DIR/META-INF"
-MANIFEST="$COMPONENT_DIR/META-INF/MANIFEST.MF"
-touch "$MANIFEST"
-echo "Bundle-Name: bc" >> "$MANIFEST"
-echo "Bundle-Symbolic-Name: ch.snipy.bc" >> "$MANIFEST"
-echo "Bundle-Version: 1.0.0-rc16" >> "$MANIFEST"
-echo 'Bundle-RequireCapability: org.graalvm; filter:="(&(graalvm_version=1.0.0-rc16)(os_arch=amd64))"' >> "$MANIFEST"
-echo "x-GraalVM-Polyglot-Part: True" >> "$MANIFEST"
+{
+    echo "Bundle-Name: bc" ;
+    echo "Bundle-Symbolic-Name: ch.snipy.bc" ;
+    echo "Bundle-Version: 1.0.0-rc16" ;
+    echo 'Bundle-RequireCapability: org.graalvm; filter:="(&(graalvm_version=1.0.0-rc16)(os_arch=amd64))"' ;
+    echo "x-GraalVM-Polyglot-Part: True"
+} > "$COMPONENT_DIR/META-INF/MANIFEST.MF"
 
-cd $COMPONENT_DIR
+(
+cd ${COMPONENT_DIR} || exit 1
 jar cfm ../bc-component.jar META-INF/MANIFEST.MF .
 
 echo "bin/bc = ../jre/languages/bc/bin/bc" > META-INF/symlinks
-if [[ $INCLUDE_BCNATIVE = "TRUE" ]]; then
+if [[ ${INCLUDE_BCNATIVE} = "TRUE" ]]; then
     echo "bin/bcnative = ../jre/languages/bc/bin/bcnative" >> META-INF/symlinks
 fi
 jar uf ../bc-component.jar META-INF/symlinks
 
-echo "jre/languages/bc/bin/bc = rwxrwxr-x" > META-INF/permissions
-echo "jre/languages/bc/bin/bcnative = rwxrwxr-x" >> META-INF/permissions
+{
+    echo "jre/languages/bc/bin/bc = rwxrwxr-x"
+    echo "jre/languages/bc/bin/bcnative = rwxrwxr-x"
+} > META-INF/permissions
+
 jar uf ../bc-component.jar META-INF/permissions
-cd ..
-rm -rf $COMPONENT_DIR
+)
+rm -rf ${COMPONENT_DIR}
--- package.sh
+++ package.sh
@@ -5,5 +5,5 @@ sed -i -e 's/RootNode root = BCParser$.MODULE$.parse(this, source);/RootNode roo
 mvn compile &&
 sed -i -e 's/RootNode root = null;/RootNode root = BCParser$.MODULE$.parse(this, source);/' $LANGFILE &&
 mvn package &&
-sudo gu uninstall ch.snipy.bc-truffle &&
+sudo gu uninstall ch.snipy.bc &&
 sudo gu -L install component/bc-component.jar
