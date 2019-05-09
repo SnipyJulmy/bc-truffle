90a690092e9c9787606f5305af326d7fc9536023
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-05-09 10:51:13 +0200

    add the literal long node

--- language/src/main/java/ch/snipy/bc/node/expression/literal/BcLongLiteralNode.java
+++ language/src/main/java/ch/snipy/bc/node/expression/literal/BcLongLiteralNode.java
@@ -1,23 +1,21 @@
 package ch.snipy.bc.node.expression.literal;
 
 import ch.snipy.bc.node.BcExpressionNode;
-import ch.snipy.bc.runtime.BcBigNumber;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.NodeInfo;
 import com.oracle.truffle.api.nodes.UnexpectedResultException;
 
-import java.math.BigDecimal;
-
 @NodeInfo(shortName = "const")
-public class BcDoubleLiteralNode extends BcExpressionNode {
-    private final BcBigNumber value;
+public class BcLongLiteralNode extends BcExpressionNode {
 
-    public BcDoubleLiteralNode(BcBigNumber value) {
+    private final long value;
+
+    public BcLongLiteralNode(long value) {
         this.value = value;
     }
 
     @Override
-    public BcBigNumber executeBigNumber(VirtualFrame frame) throws UnexpectedResultException {
+    public long executeLong(VirtualFrame frame) throws UnexpectedResultException {
         return value;
     }
 
