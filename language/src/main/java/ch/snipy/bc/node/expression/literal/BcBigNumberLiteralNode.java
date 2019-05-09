d8b2aca26c6966f7dfd33e5df31b8eb80fc07911
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-05-09 10:50:37 +0200

    add the literal BigNumber node

---
+++ language/src/main/java/ch/snipy/bc/node/expression/literal/BcBigNumberLiteralNode.java
@@ -0,0 +1,21 @@
+package ch.snipy.bc.node.expression.literal;
+
+import ch.snipy.bc.node.BcExpressionNode;
+import ch.snipy.bc.runtime.BcBigNumber;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.NodeInfo;
+
+@NodeInfo(shortName = "const")
+public final class BcBigNumberLiteralNode extends BcExpressionNode {
+
+    private final BcBigNumber value;
+
+    public BcBigNumberLiteralNode(BcBigNumber value) {
+        this.value = value;
+    }
+
+    @Override
+    public BcBigNumber executeGeneric(VirtualFrame frame) {
+        return value;
+    }
+}
