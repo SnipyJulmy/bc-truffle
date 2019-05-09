3f5b7aaf087a49ce87e744815c455b9800c790eb
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-05-09 10:42:41 +0200

    add typecast and typecheck for the bcnull value

--- language/src/main/java/ch/snipy/bc/node/BcTypes.java
+++ language/src/main/java/ch/snipy/bc/node/BcTypes.java
@@ -1,13 +1,38 @@
 package ch.snipy.bc.node;
 
 import ch.snipy.bc.runtime.BcBigNumber;
+import ch.snipy.bc.runtime.BcNull;
+import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
+import com.oracle.truffle.api.dsl.ImplicitCast;
+import com.oracle.truffle.api.dsl.TypeCast;
+import com.oracle.truffle.api.dsl.TypeCheck;
 import com.oracle.truffle.api.dsl.TypeSystem;
 
 @TypeSystem({
-        BcBigNumber.class,   // standard posix bc number
-        String.class,       // support for string
-        Object[].class     // only used to pass arguments for function call
+        long.class,
+        BcBigNumber.class,
+        String.class,
+        Object[].class
 })
 public abstract class BcTypes {
 
+    @TypeCheck(BcNull.class)
+    public static boolean isNull(Object value) {
+        return value == BcNull.SINGLETON;
+    }
+
+    @TypeCast(BcNull.class)
+    public static BcNull asBcNull(Object value) {
+        assert isNull(value);
+        return BcNull.SINGLETON;
+    }
+
+    /*
+        Implicit casting of long value to BigNumber
+     */
+    @ImplicitCast
+    @TruffleBoundary
+    public static BcBigNumber castBigNumber(long value) {
+        return BcBigNumber.valueOf(value);
+    }
 }
