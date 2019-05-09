c01a6d0d725889d7dfded4e7b256f43f3cb7d3f5
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-05-09 10:28:56 +0200

    create the null type for bc (bassically, 0...)

---
+++ language/src/main/java/ch/snipy/bc/runtime/BcNull.java
@@ -0,0 +1,31 @@
+package ch.snipy.bc.runtime;
+
+import com.oracle.truffle.api.interop.InteropLibrary;
+import com.oracle.truffle.api.interop.TruffleObject;
+import com.oracle.truffle.api.library.ExportLibrary;
+import com.oracle.truffle.api.library.ExportMessage;
+
+/*
+    In bc, the null value is represented by 0.
+    For example, if we just write
+      print a
+    without declared a yet, then it will print 0.
+ */
+@ExportLibrary(InteropLibrary.class)
+@SuppressWarnings("static-method")
+public final class BcNull implements TruffleObject {
+    public static final BcNull SINGLETON = new BcNull();
+
+    private BcNull() {
+    }
+
+    @Override
+    public String toString() {
+        return "0";
+    }
+
+    @ExportMessage
+    boolean isNull() {
+        return true;
+    }
+}
