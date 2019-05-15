a2c4d042116db6fb57601fb827ba09802e34bbaa
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-05-15 11:22:26 +0200

    refactoring

--- language/src/main/java/ch/snipy/bc/node/BcUnaryNode.java
+++ language/src/main/java/ch/snipy/bc/node/BcUnaryNode.java
@@ -4,7 +4,7 @@ import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.NodeChildren;
 
 @NodeChildren({
-        @NodeChild(value = "getValue", type = BcExpressionNode.class)
+        @NodeChild(value = "value", type = BcExpressionNode.class)
 })
 public abstract class BcUnaryNode extends BcExpressionNode {
 }
