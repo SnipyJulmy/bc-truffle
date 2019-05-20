cf748f4c30c7e66c58cc7a0600a865ce7bf65f19
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-05-20 16:20:46 +0200

    add more test

---
+++ language/src/test/resources/bc/gen_std_bc_out.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+for file in `ls | grep '\\.out'`
+do
+    rm ${file}
+done
+
+if [[ ${BC_PATH} == "" ]]; then
+    export BC_PATH="/usr/bin/bc"
+fi
+
+for file in `ls | grep '\\.bc'`
+do
+    echo ${file}
+    filename="${file%.*}"
+    ${BC_PATH} -q ${file} > ${filename}.out
+done
